d97b0577f161caad0f5d50889a5623d8d03daaa4
IKStreamIvo <ivostreaming@gmail.com>
2018-04-04 09:40:38 +0200

    I guess it's done

--- class.js
+++ class.js
@@ -9,18 +9,11 @@ var age16 = [];
 var age17 = [];
 var Sex = "Geslacht";
 var Age = "Leeftijd";
-var Origin = "Herkomstgroepering";
-var Years = "Perioden";
-var NumConvictedMinorsHalt = "TotaalAantalHaltJongeren_1";
-var NumCrimes = "TotaalMisdrijvenHalt_2";
-var ViolentCrimes = "GeweldsmisdrijvenHalt_3";
-var DestructionPubProperty = "VernielingEnOpenbareOrdeHalt_4";
-var PropertyCrimes = "VermogensmisdrijvenHalt_5";
-var OtherCrimes = "OverigeMisdrijvenHalt_6";
+var fireworkCrime = "VuurwerkovertredingenHalt_10";
+var maleAges = {"11200": 0, "11300": 0, "11400": 0, "11500": 0, "11600": 0, "11700": 0, "99   ": 0, "0    ": 0};
 
 $(document).ready(function(){
-
-    //1. get daa
+    //1. get data
     $.ajax({
         url: 'https://opendata.cbs.nl/ODataApi/odata/71930ned/TypedDataSet',
         method: 'GET',
@@ -28,8 +21,6 @@ $(document).ready(function(){
         success: function(data){
 
             dataSet = data.value;
-            filterMale();
-            filterFemale();
         },
         error: function(){
             showError();
@@ -40,42 +31,30 @@ $(document).ready(function(){
 });
 
 //2. filter data
-function filterFemale(sex){
+function filterFemale(sex, age){
     $(dataSet).each (function (index, value){
         if(value[Sex]===sex) {
-            femaleData.push(value[Sex]);
-        }
-        if (value[Age] === [11200]) {
-    age12.push(value[Age])
-}
-        if (value[Age]===[11300]) {
-            age13.push(value[Age])
-        }
-        if (value[Age]===[11400]) {
-            age14.push(value[Age])
-        }
-        if (value[Age]===[11500]) {
-            age15.push(value[Age])
-        }
-        if (value[Age]===[11600]) {
-            age16.push(value[Age])
-        }
-        if (value[Age]===[11700]) {
-            age17.push(value[Age])
+            femaleData.push(value);
         }
     });
     console.log(femaleData);
-    console.log(age12,age13,age14,age15,age16,age17);
 }
-function filterMale(sex){
+function filterMale (sex, age){
     $(dataSet).each (function (index, value){
-      if(value[Sex]===sex) {
-          maleData.push(value[Sex]);
+        if(value[Sex]===sex) {
+            maleData.push(value);
       }
     });
     console.log(maleData);
 }
 //3. counting data
+function maleCount (){
+    $(maleData).each(function(nr, value) {
+        if (value[fireworkCrime] !== null) {
+            maleAges[value[Sex]] += value[fireworkCrime];
+        }
+    })
+}
 //allows for the selection of what data you would like to see
 $("#generate").click(function(){
     if( $('#female').is(':checked'))
@@ -84,9 +63,10 @@ $("#generate").click(function(){
     }
     if( $('#male').is(':checked'))
     {
-        filterMale('3000')
+        filterMale('3000');
+        maleCount();
+        console.log(maleAges);
     }
 });
 //4. form graph(visuals)
-
 
