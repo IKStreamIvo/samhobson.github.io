d98df383fc5c1ee2908621b044161573513942b9
SamHobson <38007003+SamHobson@users.noreply.github.com>
2018-04-08 17:42:29 +0200

    kinda
    trying to css

---
+++ js/workshop5.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+    
+    getData();
+
+});
+
+
+
+function getData(url){
+
+    //Step 1. Fetch the API data and execute setupViolentCrimesTable(data)
+
+}
+
+
+function setupViolentCrimesTable(data){
+
+
+    //step 3. Get the total crimes per year rates and append the data to the HTML Table object
+
+}
+
+
+function getViolentCrimesRates(data){
+
+     //Step 2. Add the total crimes per year to an OBJECT and return it.
+
+}
+
+
+
+
